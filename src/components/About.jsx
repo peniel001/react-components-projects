2bab7faf799199dcd6d68e6ec1d156605065564d
peniel001 <penielcrypt@gmail.com>
2026-06-18 13:43:29 +0100

    Added about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,10 +8,24 @@ export default function AboutSection () {
       <div className='flex flex-1'>
         <div className='w-1/2 bg-taupe-500 flex items-center justify-center p-6'>
           <p className='text-white p-2'>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi sit,
-            perspiciatis, ipsa accusamus nulla, nostrum atque ex placeat
-            pariatur omnis harum repudiandae consequatur sint saepe quia
-            laboriosam ab consectetur porro?
+            I am a passionate fullstack web designer with versatile knowledge
+            across a wide range of programming languages and frameworks. Over
+            the years, Ive dedicated myself to building websites that are not
+            only functional but also visually striking, blending creativity with
+            technical precision. My focus is on delivering modern, user friendly
+            designs that make a lasting impression. Whether its crafting
+            responsive layouts, optimizing performance, or integrating
+            cutting edge features, I approach every project with a commitment to
+            excellence. What sets me apart is my progressive mindset — I never
+            stop learning. Technology evolves quickly, and I thrive on staying
+            ahead of the curve by exploring new tools, design trends, and
+            development practices. This drive ensures that every project I take
+            on reflects the latest innovations while meeting the unique needs of
+            clients and users. Beyond coding and design, I value collaboration
+            and clear communication. I believe the best digital experiences are
+            built when creativity and technical expertise come together
+            seamlessly. My goal is to transform ideas into impactful online
+            solutions that inspire and engage.
           </p>
         </div>
 
